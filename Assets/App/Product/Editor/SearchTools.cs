f9a2280a231c5158e512031bc041afaced8e1462
eral.r.l <eral.r.l+git@gmail.com>
2015-04-04 20:20:33 +0900

    :art:Cosmetic change
    ソース整理

--- Assets/App/Product/Editor/SearchTools.cs
+++ Assets/App/Product/Editor/SearchTools.cs
@@ -7,7 +7,6 @@ using UnityEditor;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
-using System.Reflection;
 
 public class SearchTools : EditorWindow {
 
